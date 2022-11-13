8a737a4f2cc6aa5513d84524bbca2fbe04e9b08f
madmaxFF <107015156+madmaxFF@users.noreply.github.com>
2022-11-13 09:50:42 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,11 +33,11 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
+global.owner = ['917994827702',''] //owner number +27634090203
+global.wame = ['917994827702'] 
+global.pemilik = ['917994827702'] //owner number +27634090203
+global.premium = ['917994827702'] //premium number +27634090203
+global.pengguna = 'Max' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
 global.botname = 'ZIM BOT V4'
